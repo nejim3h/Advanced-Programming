bd72858bb4e60135ecfd71214d57ab8525456ee0
Nimit Panwar <71833404+nejim3h@users.noreply.github.com>
2023-10-25 11:47:17 +0530

    Update Core.java

--- Assignment-2/src/Core.java
+++ Assignment-2/src/Core.java
@@ -46,15 +46,15 @@ public class Core {
                 }else if (adminChoice == 2) {
                     admin.manageAnimals();
                 }else if (adminChoice == 3) {
-                    admin.scheduleEvents();        // Start Coding from here !
+                    admin.scheduleEvents();        //MISSING 
                 }else if (adminChoice == 4) {
                     admin.setDiscounts();
                 }else if (adminChoice == 5) {
-                    admin.setSpecialDeals();
+                    admin.setSpecialDeals();      //MISSING
                 }else if (adminChoice == 6) {
-                    admin.viewVisitorStats();
+                    admin.viewVisitorStats();      //MISSING
                 }else if (adminChoice == 7) {
-                    admin.viewFeedback();
+                    admin.displayFeedback();          // MISSING
                 }else if (adminChoice == 8) {
                     System.out.println("Exiting...");
                     break;
@@ -91,143 +91,29 @@ public class Core {
                     System.out.println("7. Visit Attractions");
                     System.out.println("8. Leave Feedback");
                     System.out.println("9. Log Out");
-                    int visitorMenuChoice = scanner.nextInt();
+                    int visitorChoice2 = scanner.nextInt();
                     scanner.nextLine();
-                    switch (visitorMenuChoice) {
-                        case 1:
-                            System.out.println("Explore the Zoo:");
-                            System.out.println("1. View Attractions");
-                            System.out.println("2. View Animals");
-                            System.out.println("3. Exit");
-                            int exploreChoice = scanner.nextInt();
-                            scanner.nextLine();
-                            while (exploreChoice != 3) {
-                                if (exploreChoice == 1) {
-                                    System.out.println("Attractions in the Zoo:");
-                                    System.out.println("1. Jungle Safari");
-                                    System.out.println("2. Botanical Garden");
-                                    System.out.println("3. Dinosaur Show");
-                                    int attractionChoice = scanner.nextInt();
-                                    scanner.nextLine();
-                                    if (attractionChoice == 1) {
-                                        System.out.println("ZOOtopia offers an adventure ride that allows you to explore unexplored trails. Buy your ticket now!");
-                                    } else {
-                                        System.out.println("Ticket not available. Basic Members need to buy separate tickets for the attractions.");
-                                    }
-                                } else if (exploreChoice == 2) {
-                                    System.out.println("Animals in the Zoo:");
-                                    System.out.println("1. Lion");
-                                    System.out.println("2. Elephant");
-                                    int animalChoice = scanner.nextInt();
-                                    scanner.nextLine();
-                                    if (animalChoice == 1) {
-                                        System.out.println("The Lion is a mammal that belongs to the cat family.");
-                                    } else {
-                                        System.out.println("The Elephant is a mammal that is known for its large size and long trunk.");
-                                    }
-                                } else {
-                                    System.out.println("Invalid choice.");
-                                }
-                                System.out.println("Explore the Zoo:");
-                                System.out.println("1. View Attractions");
-                                System.out.println("2. View Animals");
-                                System.out.println("3. Exit");
-                                exploreChoice = scanner.nextInt();
-                                scanner.nextLine();
-                            }
-                            break;
-                        case 2:
-                            System.out.println("Buy Membership:");
-                            System.out.println("1. Basic Membership (₹20)");
-                            System.out.println("2. Premium Membership (₹50)");
-                            int membershipChoice = scanner.nextInt();
-                            scanner.nextLine();
-                            if (membershipChoice == 1) {
-                                System.out.println("Apply Discount Code: None");
-                                System.out.println("Basic Membership purchased successfully. Your balance is now ₹80.");
-                            } else if (membershipChoice == 2) {
-                                System.out.println("Apply Discount Code: None");
-                                System.out.println("Premium Membership purchased successfully. Your balance is now ₹50.");
-                            } else {
-                                System.out.println("Invalid choice.");
-                            }
-                            break;
-                        case 3:
-                            System.out.println("Buy Tickets:");
-                            System.out.println("Select an Attraction to Buy a Ticket:");
-                            System.out.println("1. Jungle Safari (₹10)");
-                            System.out.println("2. Botanical Garden (₹15)");
-                            System.out.println("3. Dinosaur Show (₹12)");
-                            int ticketChoice = scanner.nextInt();
-                            scanner.nextLine();
-                            if (ticketChoice == 1) {
-                                System.out.println("The ticket for Jungle Safari was purchased successfully. Your balance is now ₹70.");
-                            } else if (ticketChoice == 2) {
-                                System.out.println("The ticket for Botanical Garden was purchased successfully. Your balance is now ₹85.");
-                            } else if (ticketChoice == 3) {
-                                System.out.println("The ticket for Dinosaur Show was purchased successfully. Your balance is now ₹88.");
-                            } else {
-                                System.out.println("Invalid choice.");
-                            }
-                            break;
-                        case 4:
-                            System.out.println("View Discounts:");
-                            System.out.println("1. Minor (10% discount) - MINOR10");
-                            System.out.println("2. Senior Citizen (20% discount) - SENIOR20");
-                            break;
-                        case 5:
-                            System.out.println("Special Deals:");
-                            System.out.println("1. Buy 2 tickets and get 15% off");
-                            System.out.println("2. Buy 3 tickets and get 30% off");
-                            break;
-                        case 6:
-                            System.out.println("Visit Animals:");
-                            System.out.println("Select an Animal to Visit:");
-                            System.out.println("1. Lion");
-                            System.out.println("2. Elephant");
-                            int animalVisitChoice = scanner.nextInt();
-                            scanner.nextLine();
-                            if (animalVisitChoice == 1) {
-                                System.out.println("You have visited the Lion.");
-                            } else if (animalVisitChoice == 2) {
-                                System.out.println("You have visited the Elephant.");
-                            } else {
-                                System.out.println("Invalid choice.");
-                            }
-                            break;
-                        case 7:
-                            System.out.println("Visit Attractions:");
-                            System.out.println("Select an Attraction to Visit:");
-                            System.out.println("1. Jungle Safari");
-                            System.out.println("2. Botanical Garden");
-                            System.out.println("3. Dinosaur Show");
-                            int attractionVisitChoice = scanner.nextInt();
-                            scanner.nextLine();
-                            if (attractionVisitChoice == 1) {
-                                System.out.println("1 Ticket Used.");
-                                System.out.println("Thank you for visiting Jungle Safari. Hope you enjoyed the attraction.");
-                            } else if (attractionVisitChoice == 2) {
-                                System.out.println("1 Ticket Used.");
-                                System.out.println("Thank you for visiting Botanical Garden. Hope you enjoyed the attraction.");
-                            } else if (attractionVisitChoice == 3) {
-                                System.out.println("1 Ticket Used.");
-                                System.out.println("Thank you for visiting Dinosaur Show. Hope you enjoyed the attraction.");
-                            } else {
-                                System.out.println("Invalid choice.");
-                            }
-                            break;
-                        case 8:
-                            System.out.println("Leave Feedback:");
-                            System.out.println("Enter your feedback (max 200 characters):");
-                            String feedbackText = scanner.nextLine();
-                            System.out.println("Thank you for your feedback.");
-                            break;
-                        case 9:
-                            System.out.println("Logged out.");
-                            break;
-                        default:
-                            System.out.println("Invalid choice.");
-                            break;
+                    if (visitorChoice2 == 1){
+                        visitor.exploreZoo();           //INCOMPLETE ADD SELCTING ANIMALS ATTRACTIONS FURTHER
+                    }else if (visitorChoice2 == 2) {
+                        visitor.buyMembership();
+                    }else if (visitorChoice2 == 3) {
+
+                    }else if (visitorChoice2 == 4) {
+
+                    }else if (visitorChoice2 == 5) {
+
+                    }else if (visitorChoice2 == 6) {
+
+                    }else if (visitorChoice2 == 7) {
+
+                    }else if (visitorChoice2 == 8) {    //NOT PRINTING THE nAME 
+                        visitor.leaveFeedback();
+                    }else if (visitorChoice2 == 9) {
+                        System.out.println("Exiting...");
+                        break;
+                    }else {
+                        System.out.println("Enter a valid option between 1-8");
                     }
                 } else {
                     System.out.println("Invalid email or password.");
